5805c59e82443f3c9f1af67cba814016308419a7
Li <qli@ISEL-LPF3FGKR9.engr.udel.edu>
2022-07-19 13:11:09 -0400

    clean

--- actions/clean.sh
+++ actions/clean.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash 
 folder=$1
-if [ ! -d "$1"]; then 
+if [ ! -d "$1" ]; then 
 mkdir -p  "$1"
 find . -maxdepth 1  -type f | xargs -I {} mv {} "$1" -f 
-elif
+else
 echo "$1" is exist! Watch out!
 exit
 fi 
